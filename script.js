666bd917ac52a282df0ab8d0946d141edff2bee6
g-veronika <g.veronika2021@gmail.com>
2021-09-23 22:01:40 +0200

    Snake creation

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 window.onload = function() {
 
-    var canvas;
+    var canvasWidth = 900;
+    var canvasHeight = 600;
+    var blockSize = 30
     var ctx;
     var delay = 100;
-    var xCoord = 0;
-    var yCoord = 0;
+    var snakee;
 
     init();
 
     function init() {
-        canvas = document.createElement('canvas');
-        canvas.width = 900;
-        canvas.height = 600;
+        var canvas = document.createElement('canvas');
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
         canvas.style.border = "1px solid";
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
+        snakee = new Snake([[6,4], [5,4], [4,4]]);
         refreshCanvas();
     }
 
     function refreshCanvas() {
-        xCoord += 2;
-        yCoord += 2;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "#ff0000";
-        ctx.fillRect(xCoord, yCoord, 100, 50);
-        setTimeout(refreshCanvas, delay)
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        snakee.advance();     
+        snakee.draw();
+        setTimeout(refreshCanvas, delay);
+    }
+
+    function drawBlock(ctx, position) {
+        var x = position[0] * blockSize; 
+        var y = position[1] * blockSize; 
+        ctx.fillRect(x, y, blockSize, blockSize);
+    }
+
+    function Snake(body) {
+        this.body = body;
+        this.draw = function() {
+            ctx.save();
+            ctx.fillStyle = "#ff0000";
+            for(var i = 0; i < this.body.length; i++) {
+                drawBlock(ctx, this.body[i]);
+            }
+            ctx.restore();
+        };
+
+        this.advance = function() {
+            var nextPosition = this.body[0].slice();
+            nextPosition[0]++;
+            this.body.unshift(nextPosition);
+            this.body.pop();
+        }
     }
 
 }
