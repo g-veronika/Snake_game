ecc9a4c46e1b7e8e2f46fbfd4f7b81aa763b067d
g-veronika <g.veronika2021@gmail.com>
2021-09-23 20:40:34 +0200

    Refresh canvas function

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 window.onload = function() {
-    var canvas = document.createElement('canvas');
-    canvas.width = 900;
-    canvas.height = 600;
-    canvas.style.border = "1px solid";
-    document.body.appendChild(canvas);
 
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = "#ff0000";
-    ctx.fillRect(30, 30, 100, 50);
+    var canvas;
+    var ctx;
+    var delay = 100;
+    var xCoord = 0;
+    var yCoord = 0;
+
+    init();
+
+    function init() {
+        canvas = document.createElement('canvas');
+        canvas.width = 900;
+        canvas.height = 600;
+        canvas.style.border = "1px solid";
+        document.body.appendChild(canvas);
+        ctx = canvas.getContext('2d');
+        refreshCanvas();
+    }
+
+    function refreshCanvas() {
+        xCoord += 2;
+        yCoord += 2;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "#ff0000";
+        ctx.fillRect(xCoord, yCoord, 100, 50);
+        setTimeout(refreshCanvas, delay)
+    }
+
 }
